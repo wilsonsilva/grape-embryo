5f2c2cf9e3f2a2147a65ca3d41683394ee4660c3
Wilson Silva <wilson.dsigns@gmail.com>
2015-11-01 22:14:14 +0000

    Improve api spec

--- spec/api/documentation_spec.rb
+++ spec/api/documentation_spec.rb
@@ -6,7 +6,9 @@ describe Embryo::API do
       get '/documentation'
 
       expect_status(200)
-      expect_json(apiVersion: 'v1')
+      expect_json(apiVersion: 'v1',
+                  swaggerVersion: '1.2',
+                  produces: ['application/json'])
       expect_json_sizes('apis', 1)
     end
   end
