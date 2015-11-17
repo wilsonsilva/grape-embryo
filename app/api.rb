f58cdadb1fc0f9dca7dd5dfeceb36d67596413e9
Wilson Silva <wilson.dsigns@gmail.com>
2015-11-17 01:12:17 +0000

    Remove header-versioning endpoint

--- app/api.rb
+++ app/api.rb
@@ -3,7 +3,6 @@ module Embryo
   class API < Grape::API
     format :json
 
-    mount ::Embryo::HeaderVersioning
     mount ::Embryo::People
 
     add_swagger_documentation api_version: 'v1', mount_path: '/documentation'
