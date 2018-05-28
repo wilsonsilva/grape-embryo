c75913b108461657a2504ba179b315acf2a39075
Wilson Silva <wilson.dsigns@gmail.com>
2018-05-28 15:40:01 +0100

    Update dry-matcher, grape, grape-swagger and grape-active-record

--- spec/web/embryo/documentation_spec.rb
+++ spec/web/embryo/documentation_spec.rb
@@ -6,10 +6,11 @@ describe Embryo::API do
       get '/documentation'
 
       expect_status(200)
-      expect_json(apiVersion: 'v1',
-                  swaggerVersion: '1.2',
-                  produces: ['application/json'])
-      expect_json_sizes('apis', 2)
+      expect_json(
+        swagger: '2.0',
+        produces: ['application/json']
+      )
+      expect_json_sizes('paths', 2)
     end
   end
 end
